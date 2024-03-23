e1cc812af0cb4ad873075968d8b3e1d4f03285c4
Guillaume Duhan <31253241+guillaumeduhan@users.noreply.github.com>
2024-03-23 21:28:21 +0400

    commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,5 @@
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt', 'nuxt-icon'],
+  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt', 'nuxt-icon', 'nuxt-highcharts'],
   shadcn: {
     /**
      * Prefix for all the imported component
